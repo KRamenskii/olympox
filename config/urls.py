33b3fd9f20a19572018641bcaad36ac0088e0cb0
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2026-06-23 14:18:48 +0700

    Remove home prefix

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('home', include('core.urls')),
+    path('accounts', include('accounts.urls')),
+    path('', include('core.urls')),
     path('', include('testing.urls')),
 ]
 
