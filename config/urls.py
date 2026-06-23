c2e65e014b0e7103eb36d5a480d5f5e9ec208e9d
Ramenskii Kirill <kesha-1999-kirill@mail.ru>
2026-06-23 14:42:16 +0700

    Fix login redirect urls

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts', include('accounts.urls')),
+    path('', include('accounts.urls')),
     path('', include('core.urls')),
     path('', include('testing.urls')),
 ]
